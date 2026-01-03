891d7d960f206a6c16421f649842de02d15fbcaf
ashby village <jerkey@g-cipher.net>
2026-01-03 14:00:06 -0800

    need to specify soundcard for systemd to work

--- listencontinuously.sh
+++ listencontinuously.sh
@@ -3,6 +3,7 @@ LOGFILE=~/listenon28386.log
 while true; do
   (~/bin/listenon28386.sh | tee -a $LOGFILE)
   echo espeak -g5 -s90 "incoming SMS message: $(tail -n1 $LOGFILE | jq '.data.payload.text')"
-  espeak -g5 -s90 "incoming SMS message: $(tail -n1 $LOGFILE | jq '.data.payload.text')"
+  espeak -g5 -s90 "incoming SMS message: $(tail -n1 $LOGFILE | jq '.data.payload.text')" -w /tmp/espeak.wav
+  aplay -D plughw:3,0 /tmp/espeak.wav # use aplay -l to list soundcards, need to specify for systemd to work
   echo -e '\r\n'listen finished at $(date) | tee -a $LOGFILE
 done
